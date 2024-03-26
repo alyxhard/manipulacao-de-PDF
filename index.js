b4c739b70d2ba87bccaedb1f28e00253754da7d3
Alexander <alexandersilva436@gmail.com>
2024-03-26 09:48:06 -0300

    01.2V Texto alinhado ao centro

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const { PDFDocument, rgb } = require('pdf-lib'); // Importe rgb do pdf-lib
+const { PDFDocument, rgb, TextAlignment } = require('pdf-lib');
 const XLSX = require('xlsx');
 
-// Função para ler a lista de nomes do arquivo Excel
 function lerNomesDoExcel(nomeArquivo) {
     const workbook = XLSX.readFile(nomeArquivo);
     const sheetName = workbook.SheetNames[0];
@@ -11,38 +10,41 @@ function lerNomesDoExcel(nomeArquivo) {
     return nomes.map(nome => nome.nome.trim());
 }
 
-// Função para criar PDFs para cada nome na lista
 async function criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs) {
     const templateBytes = fs.readFileSync(templatePDF);
     for (const nome of listaNomes) {
         const pdfDoc = await PDFDocument.load(templateBytes);
         const pages = pdfDoc.getPages();
         const firstPage = pages[0];
+        const { width, height } = firstPage.getSize();
+        const fontSize = 17;
+        const font = await pdfDoc.embedFont('Helvetica');
+        const textWidth = font.widthOfTextAtSize(nome, fontSize);
+        const x = (width - textWidth) / 2 + 15;
+        const y = height / 2 + 130;
         firstPage.drawText(nome, {
-            x: 50,
-            y: 50,
-            size: 30,
-            color: rgb(1, 1, 1), // Defina a cor para branco
+            x: x,
+            y: y,
+            size: fontSize,
+            font: font,
+            color: rgb(1, 1, 1),
+            alignment: TextAlignment.Center // Definindo o alinhamento como centro
         });
         const novoPDFBytes = await pdfDoc.save();
         fs.writeFileSync(`${pastaPDFs}/${nome}.pdf`, novoPDFBytes);
     }
 }
 
-// Arquivo PDF de template e planilha Excel com lista de nomes
 const templatePDF = 'template.pdf';
 const listaUsuariosXLSX = 'listaUsuarios.xlsx';
 const pastaPDFs = 'pdfs';
 
-// Criar a pasta 'pdfs' se ainda não existir
 if (!fs.existsSync(pastaPDFs)) {
     fs.mkdirSync(pastaPDFs);
 }
 
-// Ler nomes do arquivo Excel
 const listaNomes = lerNomesDoExcel(listaUsuariosXLSX);
 
-// Criar PDFs para cada nome na lista
 criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs)
     .then(() => console.log('PDFs criados com sucesso!'))
     .catch(error => console.error('Erro ao criar PDFs:', error));
