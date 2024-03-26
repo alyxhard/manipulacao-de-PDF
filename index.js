c0650063e2fee82ad77f57619f75c7524c286245
Alexander <alexandersilva436@gmail.com>
2024-03-26 12:07:55 -0300

    updated

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const { PDFDocument, rgb, TextAlignment } = require('pdf-lib');
+const fontkit = require('fontkit'); // Adiciona esta linha para importar o fontkit
 const XLSX = require('xlsx');
 
-function lerNomesDoExcel(nomeArquivo) {
+async function lerNomesDoExcel(nomeArquivo) {
     const workbook = XLSX.readFile(nomeArquivo);
     const sheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[sheetName];
@@ -14,11 +15,13 @@ async function criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs) {
     const templateBytes = fs.readFileSync(templatePDF);
     for (const nome of listaNomes) {
         const pdfDoc = await PDFDocument.load(templateBytes);
+        pdfDoc.registerFontkit(fontkit); // Registra o fontkit antes de incorporar a fonte
         const pages = pdfDoc.getPages();
         const firstPage = pages[0];
         const { width, height } = firstPage.getSize();
         const fontSize = 17;
-        const font = await pdfDoc.embedFont('Helvetica');
+        const fontBytes = fs.readFileSync('fonts/Arial.ttf');
+        const font = await pdfDoc.embedFont(fontBytes);
         const textWidth = font.widthOfTextAtSize(nome, fontSize);
         const x = (width - textWidth) / 2 + 15;
         const y = height / 2 + 130;
@@ -28,7 +31,7 @@ async function criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs) {
             size: fontSize,
             font: font,
             color: rgb(1, 1, 1),
-            alignment: TextAlignment.Center // Definindo o alinhamento como centro
+            alignment: TextAlignment.Center
         });
         const novoPDFBytes = await pdfDoc.save();
         fs.writeFileSync(`${pastaPDFs}/${nome}.pdf`, novoPDFBytes);
@@ -43,8 +46,7 @@ if (!fs.existsSync(pastaPDFs)) {
     fs.mkdirSync(pastaPDFs);
 }
 
-const listaNomes = lerNomesDoExcel(listaUsuariosXLSX);
-
-criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs)
+lerNomesDoExcel(listaUsuariosXLSX)
+    .then(listaNomes => criarPDFsParaNomes(listaNomes, templatePDF, pastaPDFs))
     .then(() => console.log('PDFs criados com sucesso!'))
     .catch(error => console.error('Erro ao criar PDFs:', error));
